c1a55df2666b58d948ea7faa8db8b0081131316c
EVKae <timmyc@mail.sfsu.edu>
2018-06-23 09:37:52 -0700

    modified:   Experiment.py 	modified:   Working Non-Object Oriented Version/README.md

--- Experiment.py
+++ Experiment.py
@@ -1,1 +1,21 @@
 
+'''
+This file contains the definitions for the classes 
+	Experiment	Trial	ConePair
+Experiment will contain methods for input / output
+Trial will contain Tests and composite data
+Test will house data and methods for a pair of cones.
+'''
+
+class ConePair:
+	''' 
+	Initializes with dimension (Integer)
+	Generating Inner and Outer Cone,
+	Contains a list ConeSequence[], which 
+	'''
+	dimension = 0 # note that once dimension is changed 
+	def __init__(self,dimension):
+		self.InnerCone
+
+
+
